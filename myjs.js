27f67c566087fde6a29c351d55f7aa4d739d45eb
blarth <jminocente@gmail.com>
2021-11-30 16:28:53 -0300

    Adcionando mais events listeners, foi trocado no ultimo commit o padrao snake_case pelo camelCase

--- myjs.js
+++ myjs.js
@@ -1,17 +1,15 @@
 const pratoPrincipal = document.querySelector(".conteudo-prato-principal .pratos")
 const pratoPrincipalTodos = document.querySelectorAll(".conteudo-prato-principal .pratos")
+const bebidaPrincipal = document.querySelector(".conteudo-prato-bebida .pratos")
+const bebidaPrincipalTodos = document.querySelectorAll(".conteudo-prato-bebida .pratos")
+const sobremesaPrincipal = document.querySelector(".conteudo-prato-sobremesa .pratos")
+const sobremesaPrincipalTodos = document.querySelectorAll(".conteudo-prato-sobremesa .pratos")
 let verificadorPrato = document.querySelectorAll(".conteudo-prato-principal .clicado")
-/* function evento_click(botao) {
-  const esta_clicado = document.querySelector(
-    ".conteudo-prato-principal .clicado"
-  );
-  if (esta_clicado !== null) {
-    esta_clicado.classList.remove("clicado");
-    botao.classList.toggle("clicado");
-  } else {
-    botao.classList.toggle("clicado");
-  }
-} */
+let verificadorBebida = document.querySelectorAll(".conteudo-prato-bebida .clicado")
+let verificadorSobremesa = document.querySelectorAll(".conteudo-prato-sobremesa .clicado")
+const botaoEnviar = document.querySelector("button")
+const corpoApp = document.querySelector(".conteudo-app")
+
 for(let i = 0; i < pratoPrincipalTodos.length; i++){
   pratoPrincipalTodos[i].addEventListener("click" , (event) => {
     verificadorPrato = document.querySelectorAll(".conteudo-prato-principal .clicado")
@@ -38,22 +36,72 @@ for(let i = 0; i < pratoPrincipalTodos.length; i++){
     }
   })
 }
+for(let i = 0; i < bebidaPrincipalTodos.length; i++){
+  bebidaPrincipalTodos[i].addEventListener("click" , (event) => {
+    verificadorBebida = document.querySelectorAll(".conteudo-prato-bebida .clicado")
+    
+    
+    switch(verificadorBebida.length){
+      case 0:
+        if(event.path.length !== 7){
+          event.target.parentNode.classList.toggle("clicado")
+          break
+        }
+        event.target.classList.toggle("clicado")
+        break
+      case 1:
+        verificadorBebida[0].classList.remove("clicado")
+        if(event.path.length !== 7){
+          event.target.parentNode.classList.toggle("clicado")
+          break
+        }
+        event.target.classList.toggle("clicado")
+        break
+      default:
+        console.log("Erro nos cases.")
+    }
+  })
+}
 
-/* 
-pratoPrincipal.addEventListener("click" , (event) => {
-  console.log(pratoPrincipal)
-  console.log(verificadorPrato.length)
-  switch(verificadorPrato.length){
-    case 0:
-      event.target.classList.toggle("clicado")
-      break
-    case 1:
-      verificadorPrato[0].classList.remove("clicado")
-      event.target.classList.toggle("clicado")
-    default:
-      console.log("algo n ta dando certo")
+for(let i = 0; i < sobremesaPrincipalTodos.length; i++){
+  sobremesaPrincipalTodos[i].addEventListener("click" , (event) => {
+    verificadorSobremesa = document.querySelectorAll(".conteudo-sobremesa-principal .clicado")
+    
+    
+    switch(verificadorSobremesa.length){
+      case 0:
+        if(event.path.length !== 7){
+          event.target.parentNode.classList.toggle("clicado")
+          break
+        }
+        event.target.classList.toggle("clicado")
+        break
+      case 1:
+        verificadorSobremesa[0].classList.remove("clicado")
+        if(event.path.length !== 7){
+          event.target.parentNode.classList.toggle("clicado")
+          break
+        }
+        event.target.classList.toggle("clicado")
+        break
+      default:
+        console.log("Erro nos cases.")
+    }
+  })
+}
+
+/* function evento_click(botao) {
+  const esta_clicado = document.querySelector(
+    ".conteudo-prato-principal .clicado"
+  );
+  if (esta_clicado !== null) {
+    esta_clicado.classList.remove("clicado");
+    botao.classList.toggle("clicado");
+  } else {
+    botao.classList.toggle("clicado");
   }
-}) */
+} */
+
 
 
 /* function evento_click_bebida(botao) {
@@ -68,7 +116,7 @@ pratoPrincipal.addEventListener("click" , (event) => {
   }
 } */
 
-function evento_click_sobremesa(botao) {
+/* function evento_click_sobremesa(botao) {
   const esta_clicado_sobremesa = document.querySelector(
     ".conteudo-prato-sobremesa .clicado"
   );
@@ -79,8 +127,8 @@ function evento_click_sobremesa(botao) {
     botao.classList.toggle("clicado");
   }
 }
+ */
 
-const corpoApp = document.querySelector(".conteudo-app")
 corpoApp.addEventListener("click", () => {
   
   const pratoEscolhido = document.querySelector(
@@ -117,15 +165,73 @@ corpoApp.addEventListener("click", () => {
   const botaoPronto = document.querySelector("button");
   const mudaTextoBotao = document.querySelector(".finalizar-pedido p");
 
-  if (pratoEscolhido !== null && bebida_escolhida !== null && sobremesa_escolhida !== null) {
+  if (pratoEscolhido !== null){
+   if(bebida_escolhida !== null){
+    if(sobremesa_escolhida !== null) {
         botaoPronto.classList.add("finalizar-pedido-correto");
         mudaTextoBotao.innerHTML = "Fechar pedido";
         mudaTextoBotao.classList.add("padding");
         botaoPronto.disabled = false;  
-  
+   }
+  }
 } */
 
-function mandar_mensagem() {
+
+botaoEnviar.addEventListener("click", (event) => {
+  let pratoEscolhido = document.querySelector(
+    ".conteudo-prato-principal .clicado .titulo-prato"
+  );
+  pratoEscolhido = pratoEscolhido.innerHTML;
+
+  let bebida_escolhida = document.querySelector(
+    ".conteudo-prato-bebida .clicado .titulo-prato"
+  );
+  bebida_escolhida = bebida_escolhida.innerHTML;
+
+  let sobremesa_escolhida = document.querySelector(
+    ".conteudo-prato-sobremesa .clicado .titulo-prato"
+  );
+
+  sobremesa_escolhida = sobremesa_escolhida.innerHTML;
+
+  let valorPrato = document.querySelector(
+    ".conteudo-prato-principal .clicado .preco-prato"
+  ).innerHTML;
+  valorPrato = valorPrato.replace("R$ ", "");
+  valorPrato = valorPrato.replace(",", ".");
+  valorPrato = parseFloat(valorPrato);
+
+  let valorBebida = document.querySelector(
+    ".conteudo-prato-bebida .clicado .preco-prato"
+  ).innerHTML;
+  valorBebida = valorBebida.replace("R$ ", "");
+  valorBebida = valorBebida.replace(",", ".");
+  valorBebida = parseFloat(valorBebida);
+
+  let valorSobremesa = document.querySelector(
+    ".conteudo-prato-sobremesa .clicado .preco-prato"
+  ).innerHTML;
+  valorSobremesa = valorSobremesa.replace("R$ ", "");
+  valorSobremesa = valorSobremesa.replace(",", ".");
+  valorSobremesa = parseFloat(valorSobremesa);
+
+  let valorTotal = (num1, num2, num3) => {
+    let total = (num1 + num2 + num3)
+    total = total.toFixed(2)
+    return total 
+  }
+
+  total_msg = valorTotal(valorPrato, valorBebida, valorSobremesa)
+  
+
+  const mensagem = `Olá, gostaria de fazer o pedido:\n- Prato: ${pratoEscolhido} \n- Bebida: ${bebida_escolhida} \n- Sobremesa: ${sobremesa_escolhida} \n-Total: R$ ${total_msg} `;
+  const msg_wpp = encodeURI(mensagem);
+
+  window.location.href = `https://wa.me/5516994476161?text=${msg_wpp}`;
+
+})
+
+/* function mandar_mensagem() {
   let pratoEscolhido = document.querySelector(
     ".conteudo-prato-principal .clicado .titulo-prato"
   );
@@ -172,3 +278,4 @@ function mandar_mensagem() {
 
   window.location.href = `https://wa.me/5516994476161?text=${msg_wpp}`;
 }
+ */
